abf41ea201d64bff7b01cee81199446384e1ae45
Hernán Giayetto <hernangiayetto@gmail.com>
2020-01-06 22:20:47 -0300

    day 6

---
+++ day6/puzzle1.js
@@ -0,0 +1,47 @@
+
+const u = require('./utils.js');
+
+const input = require('./input.js');
+// const input = [
+//     'COM)B',
+//     'B)C',
+//     'C)D',
+//     'D)E',
+//     'E)F',
+//     'B)G',
+//     'G)H',
+//     'D)I',
+//     'E)J',
+//     'J)K',
+//     'K)L',
+// ];
+
+let Rta;
+
+let elements = input.slice();
+
+// console.log(elements);
+// elements.sort((a, b) => {
+//     return Math.random() - .5;
+// });
+// console.log(elements);
+
+u.title('Reordering input');
+
+let ordered = u.sortInput(elements);
+// console.log(ordered);
+
+u.title('Making sub-paths');
+
+let paths = u.makePaths(ordered);
+// console.log(paths);
+
+console.log('paths', paths.length);
+Rta = 0;
+for (let p of paths) {
+    console.log(p.toString(), '('+p.totalOrbits().toString()+')');
+    Rta += p.totalOrbits();
+}
+
+u.title('Rta ' + Rta);
+// Rta 333679
