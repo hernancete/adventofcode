7e752f0635d5d82a39b752a466148085733041fe
hernancete <hernangiayetto@gmail.com>
2023-03-22 20:15:49 -0300

    recalculate days 4 and 5 with new updated inputs

--- day4/utils.js
+++ day4/utils.js
@@ -1,38 +1,36 @@
-
-const input = [347312, 805915];
+const input = [254032, 789860];
 
 const isNeverDecreasing = (n) => {
-    let _n = n.toString();
-    for (let i=1; i<_n.length; i++) {
-        if (_n[i] < _n[i-1]) return false;
-    }
-    return true;
-}
+  let _n = n.toString();
+  for (let i = 1; i < _n.length; i++) {
+    if (_n[i] < _n[i - 1]) return false;
+  }
+  return true;
+};
 
 const isAtLeastTwoAdjacentsTheSame = (n) => {
-    let _n = n.toString();
-    for (let i=1; i<_n.length; i++) {
-        if (_n[i] == _n[i-1]) return true;
-    }
-    return false;
-}
+  let _n = n.toString();
+  for (let i = 1; i < _n.length; i++) {
+    if (_n[i] == _n[i - 1]) return true;
+  }
+  return false;
+};
 
 const isExactTwoAdjacentsTheSame = (n) => {
-    let _n = n.toString();
-    for (let i=1; i<_n.length; i++) {
-        if (_n[i] == _n[i-1]) {
-            if (i > 1 && _n[i] == _n[i-2]) continue;
-            if (i < _n.length - 1 && _n[i] == _n[i+1]) continue;
-            return true;
-        }
+  let _n = n.toString();
+  for (let i = 1; i < _n.length; i++) {
+    if (_n[i] == _n[i - 1]) {
+      if (i > 1 && _n[i] == _n[i - 2]) continue;
+      if (i < _n.length - 1 && _n[i] == _n[i + 1]) continue;
+      return true;
     }
-    return false;
-}
-
+  }
+  return false;
+};
 
 module.exports = {
-    input: input,
-    isNeverDecreasing: isNeverDecreasing,
-    isAtLeastTwoAdjacentsTheSame: isAtLeastTwoAdjacentsTheSame,
-    isExactTwoAdjacentsTheSame: isExactTwoAdjacentsTheSame,
-}
+  input: input,
+  isNeverDecreasing: isNeverDecreasing,
+  isAtLeastTwoAdjacentsTheSame: isAtLeastTwoAdjacentsTheSame,
+  isExactTwoAdjacentsTheSame: isExactTwoAdjacentsTheSame,
+};
