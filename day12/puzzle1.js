5c0be5300d6d08a4195c99962fa5de82c776d457
hernancete <hernangiayetto@gmail.com>
2023-03-22 21:07:19 -0300

    recalculate days 12 (half of it) with new updated input

--- day12/puzzle1.js
+++ day12/puzzle1.js
@@ -1,11 +1,14 @@
+const Moon = require("./utils").Moon;
 
-const Moon = require('./utils').Moon;
-
+// <x=-5, y=6, z=-11>
+// <x=-8, y=-4, z=-2>
+// <x=1, y=16, z=4>
+// <x=11, y=11, z=-4>
 const input = [
-    [16, -8, 13],
-    [4, 10, 10],
-    [17, -5, 6],
-    [13, -3, 0],
+  [-5, 6, -11],
+  [-8, -4, -2],
+  [1, 16, 4],
+  [11, 11, -4],
 ];
 
 const Io = new Moon(input[0][0], input[0][1], input[0][2]);
@@ -19,16 +22,16 @@ const Callisto = new Moon(input[3][0], input[3][1], input[3][2]);
 let Rta = 0;
 const steps = 1000;
 
-for (let s=0; s<steps; s++) {
-    Io.updateVel([Europa, Ganymede, Callisto]);
-    Europa.updateVel([Io, Ganymede, Callisto]);
-    Ganymede.updateVel([Io, Europa, Callisto]);
-    Callisto.updateVel([Io, Europa, Ganymede]);
+for (let s = 0; s < steps; s++) {
+  Io.updateVel([Europa, Ganymede, Callisto]);
+  Europa.updateVel([Io, Ganymede, Callisto]);
+  Ganymede.updateVel([Io, Europa, Callisto]);
+  Callisto.updateVel([Io, Europa, Ganymede]);
 
-    Io.updatePos();
-    Europa.updatePos();
-    Ganymede.updatePos();
-    Callisto.updatePos();
+  Io.updatePos();
+  Europa.updatePos();
+  Ganymede.updatePos();
+  Callisto.updatePos();
 }
 
 let ioE = Io.energy();
@@ -36,11 +39,11 @@ let europaE = Europa.energy();
 let ganymedeE = Ganymede.energy();
 let callistoE = Callisto.energy();
 
-console.log('Io', Io.getPos(), Io.getVel(), ioE);
-console.log('Europa', Europa.getPos(), Europa.getVel(), europaE);
-console.log('Ganymede', Ganymede.getPos(), Ganymede.getVel(), ganymedeE);
-console.log('Callisto', Callisto.getPos(), Callisto.getVel(), callistoE);
+console.log("Io", Io.getPos(), Io.getVel(), ioE);
+console.log("Europa", Europa.getPos(), Europa.getVel(), europaE);
+console.log("Ganymede", Ganymede.getPos(), Ganymede.getVel(), ganymedeE);
+console.log("Callisto", Callisto.getPos(), Callisto.getVel(), callistoE);
 Rta = ioE + europaE + ganymedeE + callistoE;
-console.log('Rta', Rta);
+console.log("Rta", Rta);
 
-// Rta 7687
+// Rta 13399
