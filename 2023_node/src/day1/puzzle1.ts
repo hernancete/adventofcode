0c0a7c547920e8fb2c211dbcf3ad625aab4c64ca
hernancete <hernangiayetto@gmail.com>
2024-11-01 17:39:27 -0300

    test: wip - reading input

---
+++ 2023_node/src/day1/puzzle1.ts
@@ -0,0 +1,9 @@
+export class Puzzle1 {
+  inputFilePath = './input.txt';
+
+  constructor() { }
+
+  readInput() {
+    return null;
+  }
+}
--- 2023_node/test/day1/puzzle1.test.ts
+++ 2023_node/test/day1/puzzle1.test.ts
@@ -1,5 +1,15 @@
+import { describe, test, expect } from '@jest/globals';
+import { Puzzle1 } from '../../src/day1/puzzle1';
+
 describe('Day1-Puzzle1', () => {
-  test('first dummy test', () => {
+  test('First dummy test', () => {
     expect(1).toBe(1);
-  })
+  });
+
+  // test('Should read the input', () => {
+  //   const puzzle1 = new Puzzle1();
+  //   const input = puzzle1.readInput();
+
+  //   expect(input).not.toBeNull();
+  // });
 });
