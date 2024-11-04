0a60b285984344b149ac3a41bdc4de48ad8ad4c3
hernancete <hernangiayetto@gmail.com>
2024-11-04 12:58:40 -0300

    feat: read input

--- 2023_node/src/day1/puzzle1.ts
+++ 2023_node/src/day1/puzzle1.ts
@@ -1,9 +1,12 @@
+import fs from 'fs';
+
 export class Puzzle1 {
-  inputFilePath = './input.txt';
 
   constructor() { }
 
-  readInput() {
-    return null;
+  readInput(inputFile: string) {
+    const input = fs.readFileSync(inputFile, 'utf8');
+    const inputLines = input.split('\n').filter(l => l);
+    return inputLines;
   }
 }
--- 2023_node/test/day1/puzzle1.test.ts
+++ 2023_node/test/day1/puzzle1.test.ts
@@ -1,15 +1,35 @@
 import { describe, test, expect } from '@jest/globals';
 import { Puzzle1 } from '../../src/day1/puzzle1';
 
-describe('Day1-Puzzle1', () => {
+import path from 'path';
+
+const inputSample = './test/day1/inputSample.txt';
+const inputSampleNoNewLine = './test/day1/inputSampleNoNewLine.txt';
+const thirdInputLine = 'ninevbmltwo69';
+
+describe('Day1 Puzzle1', () => {
   test('First dummy test', () => {
     expect(1).toBe(1);
   });
 
-  // test('Should read the input', () => {
-  //   const puzzle1 = new Puzzle1();
-  //   const input = puzzle1.readInput();
+  test('Should read the input', () => {
+    const puzzle1 = new Puzzle1();
+    const input = puzzle1.readInput(path.resolve(inputSample));
 
-  //   expect(input).not.toBeNull();
-  // });
+    expect(input).not.toBeNull();
+    expect(input).toBeInstanceOf(Array);
+    expect(input.length).toBe(10);
+    expect(input).toContain(thirdInputLine);
+  });
+
+  test('Should read the input with no ending new line', () => {
+    const puzzle1 = new Puzzle1();
+    const input = puzzle1.readInput(path.resolve(inputSampleNoNewLine));
+
+    expect(input).not.toBeNull();
+    expect(input).toBeInstanceOf(Array);
+    expect(input.length).toBe(10);
+    expect(input).toContain(thirdInputLine);
+  });
+
 });
