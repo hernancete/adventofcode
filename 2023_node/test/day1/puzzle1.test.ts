e69cbc9ff0bf5572ec6a4ea498f3995653bcf797
hernancete <hernangiayetto@gmail.com>
2024-11-04 13:03:11 -0300

    test: rearrenge tests

--- 2023_node/test/day1/puzzle1.test.ts
+++ 2023_node/test/day1/puzzle1.test.ts
@@ -12,24 +12,27 @@ describe('Day1 Puzzle1', () => {
     expect(1).toBe(1);
   });
 
-  test('Should read the input', () => {
-    const puzzle1 = new Puzzle1();
-    const input = puzzle1.readInput(path.resolve(inputSample));
+  describe('Reading input file', () => {
 
-    expect(input).not.toBeNull();
-    expect(input).toBeInstanceOf(Array);
-    expect(input.length).toBe(10);
-    expect(input).toContain(thirdInputLine);
-  });
+    test('Should read the input', () => {
+      const puzzle1 = new Puzzle1();
+      const input = puzzle1.readInput(path.resolve(inputSample));
 
-  test('Should read the input with no ending new line', () => {
-    const puzzle1 = new Puzzle1();
-    const input = puzzle1.readInput(path.resolve(inputSampleNoNewLine));
+      expect(input).not.toBeNull();
+      expect(input).toBeInstanceOf(Array);
+      expect(input.length).toBe(10);
+      expect(input).toContain(thirdInputLine);
+    });
 
-    expect(input).not.toBeNull();
-    expect(input).toBeInstanceOf(Array);
-    expect(input.length).toBe(10);
-    expect(input).toContain(thirdInputLine);
-  });
+    test('Should read the input with no ending new line', () => {
+      const puzzle1 = new Puzzle1();
+      const input = puzzle1.readInput(path.resolve(inputSampleNoNewLine));
+
+      expect(input).not.toBeNull();
+      expect(input).toBeInstanceOf(Array);
+      expect(input.length).toBe(10);
+      expect(input).toContain(thirdInputLine);
+    });
+  })
 
 });
