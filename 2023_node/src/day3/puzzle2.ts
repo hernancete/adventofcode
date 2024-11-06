ff78ad8ab99eef3097f515e7e1d377a20a08e435
hernancete <hernangiayetto@gmail.com>
2024-11-06 11:41:27 -0300

    feat: d3p2, get potential gears

--- 2023_node/src/day3/puzzle2.ts
+++ 2023_node/src/day3/puzzle2.ts
@@ -1,5 +1,23 @@
 import { Puzzle1 } from "./puzzle1";
 
+export interface gear {
+  line: number,
+  index: number,
+};
+
 export class Puzzle2 extends Puzzle1 {
 
+  gearRegex = /\*/g;
+
+  getPotentialGearsInline(lineIndex: number): Array<gear> {
+    const gears: gear[] = [];
+    const matches = this.input[lineIndex].matchAll(this.gearRegex);
+    [...matches].forEach(m => {
+      gears.push({
+        line: lineIndex,
+        index: m.index,
+      });
+    });
+    return gears;
+  }
 };
--- 2023_node/test/day3/puzzle2.test.ts
+++ 2023_node/test/day3/puzzle2.test.ts
@@ -16,3 +16,25 @@ describe('Reading the input', () => {
     expect(puzzle2.input).toContain(inputSampleThirdLine);
   });
 });
+
+describe('Looking for gears', () => {
+
+  test('Should get potential gears in a line', () => {
+    const puzzle2 = new Puzzle2(getAbsPath(__dirname, inputSample));
+    const firstLineIndex = 0;
+    const secondLineIndex = 1;
+    const fifhtLineIndex = 4;
+    const firstLineGears = puzzle2.getPotentialGearsInline(firstLineIndex);
+    const secondLineGears = puzzle2.getPotentialGearsInline(secondLineIndex);
+    const fifthLineGears = puzzle2.getPotentialGearsInline(fifhtLineIndex);
+
+    expect(firstLineGears).toBeInstanceOf(Array);
+    expect(firstLineGears).toHaveLength(0);
+    expect(secondLineGears).toBeInstanceOf(Array);
+    expect(secondLineGears).toHaveLength(1);
+    expect(secondLineGears).toEqual(expect.arrayContaining([expect.objectContaining({ line: 1, index: 3 })]));
+    expect(fifthLineGears).toBeInstanceOf(Array);
+    expect(fifthLineGears).toHaveLength(1);
+    expect(fifthLineGears).toEqual(expect.arrayContaining([expect.objectContaining({ line: 4, index: 3 })]));
+  });
+});
