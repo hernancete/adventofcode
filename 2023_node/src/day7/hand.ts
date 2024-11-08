0b3b15991f8acac7b886c5b9c501cae35fdb0714
hernancete <hernangiayetto@gmail.com>
2024-11-08 20:47:41 -0300

    feat: d7p1, infer the hand type

--- 2023_node/src/day7/hand.ts
+++ 2023_node/src/day7/hand.ts
@@ -1,7 +1,50 @@
+// FiveOfAKind:   AAAAA (groups: 1)
+// FourOfAKind:   AAAA7 (groups: 1)
+// FullHuse:      AAA33 (groups: 2)
+// ThreeOfAKind:  AAA65 (groups: 1)
+// TwoPair:       AAKK8 (groups: 2)
+// OnePair:       AA234 (groups: 1)
+// HighCard:      AKQJT (groups: 0)
+
+const cards: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+
 export class Hand {
   hand: string = '';
+  type: string = '';
 
   constructor(hand: string) {
     this.hand = hand;
+    this._inferType();
+  }
+
+  private _inferType() {
+    const orderedHand = this.hand.split('').sort((a, b) => cards.indexOf(b) - cards.indexOf(a));
+
+    const grouped: number[] = [];
+    let group = 0;
+    for (let c = 1; c < orderedHand.length; c++) {
+      if (orderedHand[c] == orderedHand[c - 1]) {
+        grouped[group] = group in grouped ? grouped[group] + 1 : 2;
+      }
+      else {
+        if (group in grouped) group++;
+      }
+    }
+    // console.log(this.hand, orderedHand, grouped);
+
+    grouped.sort((a, b) => b - a);
+    switch (grouped.length) {
+      case 0: this.type = 'HighCard'; break;
+      case 1:
+        switch (grouped[0]) {
+          case 5: this.type = 'FiveOfAKind'; break;
+          case 4: this.type = 'FourOfAKind'; break;
+          case 3: this.type = 'ThreeOfAKind'; break;
+          case 2: this.type = 'OnePair'; break;
+        };
+        break;
+      case 2:
+        this.type = grouped[0] === 3 ? 'FullHuse' : 'TwoPair';
+    }
   }
 };
--- 2023_node/test/day7/hand.test.ts
+++ 2023_node/test/day7/hand.test.ts
@@ -1,12 +1,29 @@
 import { describe, test, expect } from '@jest/globals';
 import { Hand } from '../../src/day7/hand';
 
+// FiveOfAKind:   AAAAA
+// FourOfAKind:   AAAA7
+// FullHuse:      AAA33
+// ThreeOfAKind:  AAA65
+// TwoPair:       AAKK8
+// OnePair:       AA234
+// HighCard:      AKQJT
+
 const sampleHands = [
-  { hand: '32T3K', bid: 765 },
-  { hand: 'T55J5', bid: 684 },
-  { hand: 'KK677', bid: 28 },
-  { hand: 'KTJJT', bid: 220 },
-  { hand: 'QQQJA', bid: 483 },
+  { hand: '32T3K', bid: 765, type: 'OnePair' },
+  { hand: 'T55J5', bid: 684, type: 'ThreeOfAKind' },
+  { hand: 'KK677', bid: 28, type: 'TwoPair' },
+  { hand: 'KTJJT', bid: 220, type: 'TwoPair' },
+  { hand: 'QQQJA', bid: 483, type: 'ThreeOfAKind' },
+];
+const moreSampleHands = [
+  { hand: 'AAAAA', type: 'FiveOfAKind' },
+  { hand: 'AAAA7', type: 'FourOfAKind' },
+  { hand: 'AAA33', type: 'FullHuse' },
+  { hand: 'AAA65', type: 'ThreeOfAKind' },
+  { hand: 'AAKK8', type: 'TwoPair' },
+  { hand: 'AA234', type: 'OnePair' },
+  { hand: 'AKQJT', type: 'HighCard' },
 ];
 
 describe('Creating hand', () => {
@@ -16,6 +33,14 @@ describe('Creating hand', () => {
       const hand = new Hand(h.hand);
 
       expect(hand.hand).toBe(h.hand);
+    }
+  });
+
+  test('Should find the hand type', () => {
+    for (const h of [...sampleHands, ...moreSampleHands]) {
+      const hand = new Hand(h.hand);
+
+      expect(hand.type).toBe(h.type);
     }
   });
 });
