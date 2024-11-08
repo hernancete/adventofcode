5907307263fa5a536bfae30fc2cfde02421c1cf4
hernancete <hernangiayetto@gmail.com>
2024-11-08 13:33:17 -0300

    feat: d6p1, create race with duration time

---
+++ 2023_node/src/day6/race.ts
@@ -0,0 +1,8 @@
+export class Race {
+
+  time: number;
+
+  constructor(time: number) {
+    this.time = time;
+  }
+};
---
+++ 2023_node/test/day6/race.test.ts
@@ -0,0 +1,19 @@
+import { describe, test, expect } from '@jest/globals';
+import { Race } from '../../src/day6/race';
+
+const raceSamples = [
+  { time: 7, record: 9, winningOptions: [2, 3, 4, 5] },
+  { time: 15, record: 40, winningOptions: [4, 5, 6, 7, 8, 9, 10, 11] },
+  { time: 30, record: 200, winningOptions: [11, 12, 13, 14, 15, 16, 17, 18, 19] },
+];
+
+describe('Creating races', () => {
+
+  test('Should create an instance of race with time duration', () => {
+    for (const raceSample of raceSamples) {
+      const race = new Race(raceSample.time);
+
+      expect(race.time).toBe(raceSample.time);
+    }
+  });
+});
