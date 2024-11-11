1cb6918e9e442e2a091e3b69fede0598cdca437b
hernancete <hernangiayetto@gmail.com>
2024-11-11 15:55:40 -0300

    feat: d7p2, find the hand type

--- 2023_node/src/day7/jokerHand.ts
+++ 2023_node/src/day7/jokerHand.ts
@@ -1,5 +1,5 @@
 
-// export const cardsOrder: string[] = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
+export const cardsOrder: string[] = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
 
 export class JokerHand {
 
@@ -9,9 +9,58 @@ export class JokerHand {
 
   constructor(hand: string) {
     this.hand = hand;
+    this._inferType();
   }
 
   loadBid(bid: number) {
     this.bid = bid;
+  }
+
+  private _inferType() {
+    const noJokerHand = this.hand.replace(/J/g, '');
+    const jokers = this.hand.length - noJokerHand.length;
+    const orderedNoJokerHand = noJokerHand.split('').sort((a, b) => cardsOrder.indexOf(b) - cardsOrder.indexOf(a));
+
+    const grouped: number[] = [];
+    let group = 0;
+    for (let c = 1; c < orderedNoJokerHand.length; c++) {
+      if (orderedNoJokerHand[c] == orderedNoJokerHand[c - 1]) {
+        grouped[group] = group in grouped ? grouped[group] + 1 : 2;
+      }
+      else {
+        if (group in grouped) group++;
+      }
+    }
+
+    grouped.sort((a, b) => b - a);
+
+    if (grouped.length === 0) {
+      // No grouped means every no-joker card is different. That being said:
+      // 0 jokers mean high card
+      // 1 joker means one pair
+      // 2 jokers mean three of a kind
+      // 3 jokers mean four of a kind
+      // 4 jokers mean five of a kind
+      // 5 jokers mean five of a kind
+      if (jokers) grouped[0] = jokers === 5 ? jokers : 1 + jokers;
+    }
+    else {
+      // If I have groups, the jokers add to the first (bigest) to make an even bigger group
+      grouped[0] = grouped[0] + jokers;
+    }
+
+    switch (grouped.length) {
+      case 0: this.type = 'HighCard'; break;
+      case 1:
+        switch (grouped[0]) {
+          case 5: this.type = 'FiveOfAKind'; break;
+          case 4: this.type = 'FourOfAKind'; break;
+          case 3: this.type = 'ThreeOfAKind'; break;
+          case 2: this.type = 'OnePair'; break;
+        };
+        break;
+      case 2:
+        this.type = grouped[0] === 3 ? 'FullHuse' : 'TwoPair';
+    }
   }
 };
--- 2023_node/test/day7/jokerHand.test.ts
+++ 2023_node/test/day7/jokerHand.test.ts
@@ -8,6 +8,33 @@ const sampleHands = [
   { hand: 'KTJJT', bid: 220, type: 'FourOfAKind', ordered: 'KTTJJ' },
   { hand: 'QQQJA', bid: 483, type: 'FourOfAKind', ordered: 'AQQQJ' },
 ];
+const moreSampleHands = [
+  { hand: 'AAAAA', type: 'FiveOfAKind' },
+  { hand: 'AAAA7', type: 'FourOfAKind' },
+  { hand: 'AAA33', type: 'FullHuse' },
+  { hand: 'AAA65', type: 'ThreeOfAKind' },
+  { hand: 'AAKK8', type: 'TwoPair' },
+  { hand: 'AA234', type: 'OnePair' },
+  { hand: 'AKQJT', type: 'OnePair' },
+  { hand: '55555', type: 'FiveOfAKind' },
+  { hand: '99K99', type: 'FourOfAKind' },
+  { hand: '23232', type: 'FullHuse' },
+  { hand: '4JJ3J', type: 'FourOfAKind' },
+  { hand: '8QQA8', type: 'TwoPair' },
+  { hand: '796K6', type: 'OnePair' },
+  { hand: '9843Q', type: 'HighCard' },
+  { hand: 'JJJJJ', type: 'FiveOfAKind' },
+  { hand: '3JJJJ', type: 'FiveOfAKind' },
+  { hand: '33JJJ', type: 'FiveOfAKind' },
+  { hand: '34JJJ', type: 'FourOfAKind' },
+  { hand: '333JJ', type: 'FiveOfAKind' },
+  { hand: '334JJ', type: 'FourOfAKind' },
+  { hand: '345JJ', type: 'ThreeOfAKind' },
+  { hand: '3333J', type: 'FiveOfAKind' },
+  { hand: '3334J', type: 'FourOfAKind' },
+  { hand: '3344J', type: 'FullHuse' },
+  { hand: '3456J', type: 'OnePair' },
+];
 
 describe('Creating joker hand', () => {
 
@@ -33,6 +60,14 @@ describe('Creating joker hand', () => {
       const hand = new JokerHand(h.hand);
 
       expect(hand.bid).toBe(0);
+    }
+  });
+
+  test('Should find the hand type', () => {
+    for (const h of [...sampleHands, ...moreSampleHands]) {
+      const hand = new JokerHand(h.hand);
+
+      expect(hand.type).toBe(h.type);
     }
   });
 });
