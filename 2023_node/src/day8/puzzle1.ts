5b2999a1ad22ad9299ffd458ff51ad76ddecc930
hernancete <hernangiayetto@gmail.com>
2024-11-11 17:40:01 -0300

    feat: d8p1, parse input

---
+++ 2023_node/src/day8/puzzle1.ts
@@ -0,0 +1,21 @@
+import { Puzzle } from "../shared/puzzle";
+
+export class Puzzle1 extends Puzzle {
+
+  pattern: string[] = [];
+  network: any = {};
+
+  constructor(inputFile: string) {
+    super(inputFile);
+    this.pattern = this.input[0].split('');
+    this._parseNetwork();
+  }
+
+  private _parseNetwork() {
+    this.input.slice(1).forEach(i => {
+      // AAA = (BBB, CCC)
+      const [nameMatch, lMatch, rMatch] = [...i.matchAll(/([A-Z]{3})/g)];
+      this.network[nameMatch[0]] = { L: lMatch[0], R: rMatch[0] };
+    })
+  }
+};
