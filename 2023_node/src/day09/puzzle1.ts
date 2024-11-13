bfbb3a8cfaa9e15e5b55f5269116f093c16691dc
hernancete <hernangiayetto@gmail.com>
2024-11-13 15:48:34 -0300

    feat: d9p1, calculate difference between history values

--- 2023_node/src/day09/puzzle1.ts
+++ 2023_node/src/day09/puzzle1.ts
@@ -3,6 +3,7 @@ import { Puzzle } from "../shared/puzzle";
 export class Puzzle1 extends Puzzle {
 
   history: number[][] = [];
+  historyDiffsLastDigits: number[][] = [];
 
   constructor(inputFile: string) {
     super(inputFile);
@@ -13,5 +14,28 @@ export class Puzzle1 extends Puzzle {
     this.input.forEach(i => {
       this.history.push(i.split(/ +/g).map(n => parseInt(n)));
     });
+  }
+
+  calculateDiffsBetweenConsecutiveValues(values: number[]): number[] {
+    if (values.length < 2) return values;
+    const diff: number[] = [];
+    for (let i = 1; i < values.length; i++) {
+      diff.push(values[i] - values[i - 1]);
+    }
+    return diff;
+  }
+
+  checkIfEveryValueIsZero(values: number[]): boolean {
+    return values.every(v => v === 0);
+  }
+
+  getTheDiffBetweenTheLastTwoValuesOfEachDiffSequence(historyIndex: number) {
+    let diffs: number[] = [...this.history[historyIndex]];
+    while (true) {
+      diffs = this.calculateDiffsBetweenConsecutiveValues(diffs);
+      if (this.checkIfEveryValueIsZero(diffs)) break;
+      if (!this.historyDiffsLastDigits[historyIndex]) this.historyDiffsLastDigits[historyIndex] = [];
+      this.historyDiffsLastDigits[historyIndex].push(diffs.at(-1)!);
+    }
   }
 };
--- 2023_node/test/day09/puzzle1.test.ts
+++ 2023_node/test/day09/puzzle1.test.ts
@@ -16,3 +16,46 @@ describe('Parsing the input', () => {
     expect(puzzle1.history[2]).toEqual(expect.arrayContaining([21, 30, 45]));
   });
 });
+
+describe('Forecasting the next history value', () => {
+
+  test('Should calculate the difference between every two consecutive history values', () => {
+    const puzzle1 = new Puzzle1(getAbsPath(__dirname, inputSample));
+    let diffs: number[];
+
+    diffs = puzzle1.calculateDiffsBetweenConsecutiveValues([0, 3, 6, 9, 12, 15]);
+    expect(diffs).toEqual(expect.arrayContaining([3, 3, 3, 3, 3]));
+    diffs = puzzle1.calculateDiffsBetweenConsecutiveValues([3, 3, 3, 3, 3]);
+    expect(diffs).toEqual(expect.arrayContaining([0, 0, 0, 0]));
+
+    diffs = puzzle1.calculateDiffsBetweenConsecutiveValues([1, 3, 6, 10, 15, 21]);
+    expect(diffs).toEqual(expect.arrayContaining([2, 3, 4, 5, 6]));
+    diffs = puzzle1.calculateDiffsBetweenConsecutiveValues([2, 3, 4, 5, 6]);
+    expect(diffs).toEqual(expect.arrayContaining([1, 1, 1, 1]));
+    diffs = puzzle1.calculateDiffsBetweenConsecutiveValues([1, 1, 1, 1]);
+    expect(diffs).toEqual(expect.arrayContaining([0, 0, 0]));
+  });
+
+  test('Should check if every value of a set is zero', () => {
+    const puzzle1 = new Puzzle1(getAbsPath(__dirname, inputSample));
+
+    expect(puzzle1.checkIfEveryValueIsZero([0, 3, 6, 9, 12, 15])).toEqual(false);
+    expect(puzzle1.checkIfEveryValueIsZero([3, 3, 3, 3, 3])).toEqual(false);
+    expect(puzzle1.checkIfEveryValueIsZero([0, 0, 0, 0])).toEqual(true);
+    expect(puzzle1.checkIfEveryValueIsZero([1, 3, 6, 10, 15, 21])).toEqual(false);
+    expect(puzzle1.checkIfEveryValueIsZero([2, 3, 4, 5, 6])).toEqual(false);
+    expect(puzzle1.checkIfEveryValueIsZero([1, 1, 1, 1])).toEqual(false);
+    expect(puzzle1.checkIfEveryValueIsZero([0, 0, 0])).toEqual(true);
+  });
+
+  test('Should get the last digit of every set of differences', () => {
+    const puzzle1 = new Puzzle1(getAbsPath(__dirname, inputSample));
+    puzzle1.getTheDiffBetweenTheLastTwoValuesOfEachDiffSequence(0);
+    puzzle1.getTheDiffBetweenTheLastTwoValuesOfEachDiffSequence(1);
+    puzzle1.getTheDiffBetweenTheLastTwoValuesOfEachDiffSequence(2);
+
+    expect(puzzle1.historyDiffsLastDigits[0]).toEqual(expect.arrayContaining([3]));
+    expect(puzzle1.historyDiffsLastDigits[1]).toEqual(expect.arrayContaining([6, 1]));
+    expect(puzzle1.historyDiffsLastDigits[2]).toEqual(expect.arrayContaining([15, 6, 2]));
+  });
+});
