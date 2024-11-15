da5df1537d97764882c4df8a6fe1f81e5d5d0efb
hernancete <hernangiayetto@gmail.com>
2024-11-15 15:46:44 -0300

    feat: d10p2, fill the non-pipe tiles of land with tiles

--- 2023_node/src/day10/puzzle2.ts
+++ 2023_node/src/day10/puzzle2.ts
@@ -1,5 +1,6 @@
 import { Puzzle1 } from "./puzzle1";
 import { PipeTile } from "./pipeTile";
+import { Tile } from "./tile";
 
 export class Puzzle2 extends Puzzle1 {
 
@@ -11,5 +12,15 @@ export class Puzzle2 extends Puzzle1 {
       if (!Array.isArray(this.land[location.lat])) this.land[location.lat] = [];
       this.land[location.lat][location.lon] = new PipeTile(this.input[location.lat][location.lon], { lat: location.lat, lon: location.lon });
     });
+  }
+
+  fillLandWithTiles() {
+    for (let lat = 0; lat < this.input.length; lat++) {
+      if (!Array.isArray(this.land[lat])) this.land[lat] = [];
+      for (let lon = 0; lon < this.input[lat].length; lon++) {
+        if (this.land[lat][lon] && this.land[lat][lon] instanceof PipeTile) continue;
+        this.land[lat][lon] = new Tile({ lat, lon });
+      }
+    }
   }
 };
--- 2023_node/test/day10/puzzle2.test.ts
+++ 2023_node/test/day10/puzzle2.test.ts
@@ -2,8 +2,10 @@ import { describe, test, expect } from '@jest/globals';
 import { getAbsPath } from '../utils'
 import { Puzzle2 } from '../../src/day10/puzzle2';
 import { PipeTile } from '../../src/day10/pipeTile';
+import { Tile } from '../../src/day10/tile';
 
 const inputSampleB = getAbsPath(__dirname, './input2.txt');
+const inputSampleC = getAbsPath(__dirname, './input3.txt');
 const inputSampleBPipeTileLocations = [
   [false, false, true, true, false],
   [false, true, true, true, false],
@@ -11,6 +13,18 @@ const inputSampleBPipeTileLocations = [
   [true, true, true, true, true],
   [true, true, false, false, false],
 ]
+const inputSampleCPipeTileLocations = [
+  ['O', 'P', 'P', 'P', 'S', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'O'],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'I', 'P', 'P', 'P', 'P', 'O'],
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'I', 'I', 'I', 'I', 'P', 'P', 'P', 'P', 'O', 'O'],
+  ['O', 'O', 'O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'I', 'I', 'I', 'P', 'P', 'O', 'O', 'O', 'O'],
+  ['O', 'O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'I', 'I', 'P', 'P', 'P', 'P', 'P'],
+  ['O', 'O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['O', 'O', 'O', 'O', 'O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+  ['O', 'O', 'O', 'O', 'O', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'O', 'O'],
+];
 
 describe('Building the whole land as individual tiles', () => {
 
@@ -25,6 +39,37 @@ describe('Building the whole land as individual tiles', () => {
         }
         else {
           expect(puzzle2.land[lat][lon]).not.toBeInstanceOf(PipeTile);
+        }
+      }
+    }
+  });
+
+  test('Should mark every tile not belonging the main loop as a tile', () => {
+    const puzzle2B = new Puzzle2(inputSampleB);
+    puzzle2B.fillLandWithPipeTiles();
+    puzzle2B.fillLandWithTiles();
+
+    const puzzle2C = new Puzzle2(inputSampleC);
+    puzzle2C.fillLandWithPipeTiles();
+    puzzle2C.fillLandWithTiles();
+
+    for (const lat in inputSampleBPipeTileLocations) {
+      for (const lon in inputSampleBPipeTileLocations[lat]) {
+        if (inputSampleBPipeTileLocations[lat][lon]) {
+          expect(puzzle2B.land[lat][lon]).toBeInstanceOf(PipeTile);
+        }
+        else {
+          expect(puzzle2B.land[lat][lon]).toBeInstanceOf(Tile);
+        }
+      }
+    }
+    for (const lat in inputSampleCPipeTileLocations) {
+      for (const lon in inputSampleCPipeTileLocations[lat]) {
+        if (['P', 'S'].includes(inputSampleCPipeTileLocations[lat][lon])) {
+          expect(puzzle2C.land[lat][lon]).toBeInstanceOf(PipeTile);
+        }
+        else {
+          expect(puzzle2C.land[lat][lon]).toBeInstanceOf(Tile);
         }
       }
     }
