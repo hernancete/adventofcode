56a0a4991ab46b702ecb2c1056e0b2bdc59d582f
hernancete <hernangiayetto@gmail.com>
2024-11-27 17:30:17 -0300

    feat: d11p1, expand the universe

--- 2023_node/src/day11/puzzle1.ts
+++ 2023_node/src/day11/puzzle1.ts
@@ -2,4 +2,36 @@ import { Puzzle } from "../shared/puzzle";
 
 export class Puzzle1 extends Puzzle {
 
+  universe: string[][] = [];
+
+  shouldExpandVertically(): number[] {
+    const indexesToExpand: number[] = this.input.reduce((prev, curr, index) => {
+      if (curr.replace(/\./g, '') === '') prev.push(index);
+      return prev;
+    }, [] as number[]);
+    return indexesToExpand;
+  }
+
+  shouldExpandHorizontally(): number[] {
+    const indexesToExpand: number[] = this.input[0].split('').map((_, i) => i);
+    this.input.forEach(row => {
+      [...row.matchAll(/#/g)].map(m => {
+        const remove = indexesToExpand.indexOf(m.index);
+        if (remove !== -1) indexesToExpand.splice(remove, 1);
+      });
+    });
+    return indexesToExpand;
+  }
+
+  expand() {
+    this.universe = this.input.map(row => row.split(''));
+    const verticallyExpandIndexes = this.shouldExpandVertically();
+    const horizontallyExpandIndexes = this.shouldExpandHorizontally();
+    verticallyExpandIndexes.reverse().forEach(index => this.universe.splice(index, 0, this.universe[index]));
+    horizontallyExpandIndexes.reverse().forEach(index => {
+      for (let row = 0; row < this.universe.length; row++) {
+        this.universe[row].splice(index, 0, this.universe[row][index]);
+      }
+    });
+  }
 };
--- 2023_node/test/day11/puzzle1.test.ts
+++ 2023_node/test/day11/puzzle1.test.ts
@@ -3,6 +3,14 @@ import { getAbsPath } from '../utils';
 import { Puzzle1 } from '../../src/day11/puzzle1';
 
 const sampleA = getAbsPath(__dirname, './input.txt');
+const sampleAVerticallyExpandIndexes = [3, 7];
+const sampleAVerticallyNotExpandIndexes = [0, 1, 2, 4, 5, 6, 8, 9];
+const sampleAHorizontallyExpandIndexes = [2, 5, 8];
+const sampleAHorizontallyNotExpandIndexes = [0, 1, 3, 4, 6, 7, 9];
+const sampleAExpandedUniverseRow0 = '....#........';
+const sampleAExpandedUniverseRow1 = '.........#...';
+const sampleAExpandedUniverseRow6 = '.#...........';
+const sampleAExpandedUniverseRow11 = '#....#.......';
 
 describe('Parsing the input', () => {
 
@@ -14,5 +22,36 @@ describe('Parsing the input', () => {
     expect(typeof puzzle1.input[0]).toBe('string');
     expect(puzzle1.input[0]).toHaveLength(10);
     expect(puzzle1.input[0]).toMatch(/^[\.#]{10}$/);
+  });
+});
+
+describe('Expanding the universe', () => {
+
+  test('Should expand vertically', () => {
+    const puzzle1 = new Puzzle1(sampleA);
+    const verticallyExpandIndexes = puzzle1.shouldExpandVertically();
+    expect(verticallyExpandIndexes).toEqual(expect.arrayContaining(sampleAVerticallyExpandIndexes));
+    expect(verticallyExpandIndexes).toEqual(expect.not.arrayContaining(sampleAVerticallyNotExpandIndexes));
+  });
+
+  test('Should expand horizontally', () => {
+    const puzzle1 = new Puzzle1(sampleA);
+    const horizontallyExpandIndexes = puzzle1.shouldExpandHorizontally();
+    expect(horizontallyExpandIndexes).toEqual(expect.arrayContaining(sampleAHorizontallyExpandIndexes));
+    expect(horizontallyExpandIndexes).toEqual(expect.not.arrayContaining(sampleAHorizontallyNotExpandIndexes));
+  });
+
+  test('Should expand the universe', () => {
+    const puzzle1 = new Puzzle1(sampleA);
+    puzzle1.expand();
+
+    expect(puzzle1.universe).toBeInstanceOf(Array);
+    expect(puzzle1.universe).toHaveLength(12);
+    expect(puzzle1.universe[0]).toBeInstanceOf(Array);
+    expect(puzzle1.universe[0]).toHaveLength(13);
+    expect(puzzle1.universe[0]).toEqual(sampleAExpandedUniverseRow0.split(''));
+    expect(puzzle1.universe[1]).toEqual(sampleAExpandedUniverseRow1.split(''));
+    expect(puzzle1.universe[6]).toEqual(sampleAExpandedUniverseRow6.split(''));
+    expect(puzzle1.universe[11]).toEqual(sampleAExpandedUniverseRow11.split(''));
   });
 });
