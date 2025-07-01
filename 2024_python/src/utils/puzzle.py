4dd83f3c001c9dffef12b944f2c01066fb2735ca
hernancete <hernangiayetto@gmail.com>
2025-07-01 14:01:25 -0300

    feat(utils): generate matrix with input in puzzle

--- 2024_python/src/utils/puzzle.py
+++ 2024_python/src/utils/puzzle.py
@@ -7,5 +7,10 @@ class Puzzle:
         with open(self.input_file, 'r') as file:
             self.lines = [line.strip() for line in file]
 
+    def _lines_to_matrix(self):
+        self.matrix = [list(line) for line in self.lines]
+        self.h = len(self.matrix)
+        self.w = len(self.matrix[0])
+
     def solve(self):
         raise NotImplementedError
